8300d6f6c4b3934d73e5d4ef0ddde0f327c689ac
nomad-1465 <nomad1465QS@gmail.com>
2023-05-08 22:45:10 +0600

    MyHashTable operations:insertion,searching,deletion:)

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.util.Hashtable;
+
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        Hashtable<Integer,String>table1=new Hashtable<>();
+        MyHashTable table=new MyHashTable(10);
+        table.insert("Sayat","Qusain");
+        table.insert("Aidos","Tinishtybai");
+        table.insert("Ermek","Tnaliev");
+        table.delete("Sayat");
+        System.out.println(table.get("Sayat"));
     }
 }
---
+++ src/MyHashTable.java
@@ -0,0 +1,65 @@
+import java.util.LinkedList;
+
+public class MyHashTable {
+
+
+    private int size;
+    private LinkedList<Node>[]table;
+
+    private class Node{
+        private Object key;
+        private Object value;
+        public Node(Object key,Object value){
+            this.key=key;
+            this.value=value;
+        }
+    }
+    public MyHashTable(int size){
+        this.size=size;
+        table=new LinkedList[size];
+        for(int i=0;i<size;i++){
+            table[i]=new LinkedList<>();
+        }
+    }
+
+    private int hash_function(Object key){
+        int hash=key.hashCode()%size;
+        if (hash < 0) {
+            hash += size;
+        }
+        return hash;
+    }
+
+    public void insert(Object key, Object value){
+        int index=hash_function(key);
+        LinkedList<Node>list=table[index];
+        for(Node node:list){
+            if(node.key.equals(key)){
+                node.value=value;
+                return;
+            }
+        }
+        list.add(new Node(key,value));
+    }
+    public Object get(Object key){
+        int index=hash_function(key);
+        LinkedList<Node>list=table[index];
+        for(Node node:list){
+            if(node.key.equals(key)){
+                return node.value;
+            }
+        }
+        return "Nothing found with this key";
+    }
+
+    public void delete(Object key){
+        int index=hash_function(key);
+        LinkedList<Node>list=table[index];
+        for(Node node:list){
+            if(node.key.equals(key)){
+                list.remove(node);
+                return;
+            }
+        }
+    }
+}
